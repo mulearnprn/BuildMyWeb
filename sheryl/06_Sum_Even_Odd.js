100e2c97b9382273a8b7c8b2d469d1b9253d304d
sherylmariam <sherylmariam123@gmail.com>
2025-06-01 19:52:58 +0530

    Create 06_Sum_Even_Odd.js

---
+++ sheryl/06_Sum_Even_Odd.js
@@ -0,0 +1,25 @@
+let result;
+function sumOrDifference(num1,num2){
+    if(num1 % 2 == 0 && num2 % 2 == 0){
+        result = num1 + num2;
+        return result;
+    }
+    else if(num1 % 2 != 0 && num2 % 2 != 0){
+        result = num1 - num2;
+        return result;
+    }
+    else 
+        return -1;
+    
+}
+
+
+
+
+//for testing outputs
+console.log(sumOrDifference(2, 4)); 
+// Expected Output: 6 (Even sum)
+console.log(sumOrDifference(3, 5)); 
+// Expected Output: -2 (Odd difference)
+console.log(sumOrDifference(2, 3)); 
+// Expected Output: -1 (One even, one odd)
