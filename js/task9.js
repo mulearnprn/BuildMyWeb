6f3ffd03f6f66aeddc7ac78c682f733ed12aa476
Abdel Baasit <baasitjaber@gmail.com>
2025-06-05 08:49:50 +0530

    Create task9.js

---
+++ js/task9.js
@@ -0,0 +1,12 @@
+// Question 9: Write an arrow function that accepts two numbers and returns their product. 
+// Use an arrow function to define it.
+function multiply(x,y) {
+    return x*y;
+}
+
+
+
+console.log(multiply(4, 5)); 
+// Expected Output: 20
+console.log(multiply(2, 3)); 
+// Expected Output: 6
